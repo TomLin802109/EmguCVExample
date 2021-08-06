1ae9344b0f63eabd7b61277afbdc99389cfa6ec0
neihu802109 <neihu802109@gmail.com>
2021-08-06 17:53:08 +0800

    Add subwindow design to show other user control into new window when main window running

---
+++ EmguCVExampleWPF/SubWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Quadrep
+{
+    /// <summary>
+    /// Interaction logic for SubWindow.xaml
+    /// </summary>
+    public partial class SubWindow : Window
+    {
+        public SubWindow(UIElement uIElement, string windowName = "")
+        {
+            InitializeComponent();
+            if (!windowName.Equals("")) this.Title = windowName;
+            MainGrid.Children.Add(uIElement);
+        }
+    }
+}
